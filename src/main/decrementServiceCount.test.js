e87e101374655eff2e13eb09132aa79348972197
Chikara <chtakahashi10@gmail.com>
2021-09-16 14:37:26 -0400

    add repo-hash

--- src/main/decrementServiceCount.test.js
+++ src/main/decrementServiceCount.test.js
@@ -5,6 +5,7 @@ jest.mock('@actions/core');
 
 const FAKE_SERVICE = 'fake-service'
 const FAKE_CLUSTER = 'fake-cluster'
+const FAKE_REPO_HASH = 'fake-hash'
 
 function mockGetInput(requestResponse) {
   return function (name, options) { // eslint-disable-line no-unused-vars
@@ -14,10 +15,16 @@ function mockGetInput(requestResponse) {
 
 const NO_INPUTS = {}
 const SERVICE_ONLY = {
-  service: FAKE_SERVICE
+  service: FAKE_SERVICE,
+  'repository-hash': ''
 }
 const CLUSTER_ONLY = {
-  cluster: FAKE_CLUSTER
+  cluster: FAKE_CLUSTER,
+  'repository-hash': ''
+}
+
+const REPO_HASH_ONLY = {
+  'repository-hash': FAKE_REPO_HASH
 }
 const ALL_INPUTS = {
   service: FAKE_SERVICE,
@@ -83,5 +90,14 @@ describe('Decrement Service Count', () => {
     await run();
     expect(mockUpdateService).toHaveBeenCalledTimes(1)
     expect(mockUpdateService).toHaveBeenNthCalledWith(1, {'service': FAKE_SERVICE, 'cluster': FAKE_CLUSTER, 'desiredCount': 0})
+  })
+
+  test('when a valid repo hash is provided, function runs successfully', async () => {
+    core.getInput = jest
+      .fn()
+      .mockImplementation(mockGetInput(REPO_HASH_ONLY))
+      await run();
+      expect(mockUpdateService).toHaveBeenCalledTimes(1)
+      expect(mockUpdateService).toHaveBeenNthCalledWith(1, {'service': `gh-runner-${FAKE_REPO_HASH}`, 'cluster': `gh-runner-${FAKE_REPO_HASH}`, 'desiredCount': 0})
   })
 });
